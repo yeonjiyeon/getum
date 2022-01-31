a17fa4d67f1d5f212c64105dffa7ebadb4631a15
yeonjiyeon <dalnim95@naver.com>
2022-01-31 18:16:00 +0900

    project init

---
+++ src/main/java/shop/getum/GetumApplication.java
@@ -0,0 +1,13 @@
+package shop.getum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetumApplication.class, args);
+	}
+
+}
