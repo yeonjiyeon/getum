cd54a39fa402136ced59be893d6803d1b30fb356
yeonjiyeon <dalnim95@naver.com>
2022-03-19 23:10:43 +0900

    Feat: 로그인 API 개발 진행 중

--- src/main/java/shop/getum/service/MemberService.java
+++ src/main/java/shop/getum/service/MemberService.java
@@ -1,4 +1,22 @@
 package shop.getum.service;
 
+import shop.getum.domain.Member;
+
 public interface MemberService {
+
+    //회원가입(회원 생성)
+    public Long join(Member member);
+
+    //로그인(단일 회원 조회)
+    public Member login();
+
+    //회원 수정
+    public void updateMember();
+
+
+    //회원 삭제
+    public void deleteMember();
+
+    //전체 회원 조회(관리자 기능)
+    public void retireMembers();
 }
