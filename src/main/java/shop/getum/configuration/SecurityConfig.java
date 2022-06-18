9b52e1a91b867127bc50f88880e45886887492c2
yeonjiyeon <dalnim95@naver.com>
2022-06-18 13:28:02 +0900

    feat security 설정 추가

--- src/main/java/shop/getum/configuration/SecurityConfig.java
+++ src/main/java/shop/getum/configuration/SecurityConfig.java
@@ -3,18 +3,26 @@ package shop.getum.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity//직접 시큐리티 설정을 위해 사용
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {//추가적인 시큐리트 기능을 쓰기 위해 상속받는다
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers("/h2-console/**", "/favicon.ico");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests()//HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다는 의미
+            .antMatchers("/api/hello").permitAll()//해당 url에 대한 요청은 인증없이 접근을 허용하겠다는 의미
             .mvcMatchers("/", "/login", "/member", "/check-email", "/check-email-token",
                 "/email-login", "/check-email-login", "/login-link").permitAll()
-            .anyRequest().authenticated();
+            .anyRequest().authenticated();//나머지 요청은 인증 받아야 한다는 의미
     }
 }
--- src/main/java/shop/getum/domain/member/Member.java
+++ src/main/java/shop/getum/domain/member/Member.java
@@ -1,11 +1,18 @@
 package shop.getum.domain.member;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Set;
 import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import shop.getum.domain.Authority;
 import shop.getum.domain.common.Address;
 import shop.getum.domain.common.BaseEntity;
 
@@ -16,12 +23,14 @@ import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity//데이터베이스 테이블로 1대 1 매핑되는 객체
 @Getter
 @Builder
 @EqualsAndHashCode(of = "id")
+@Table(name = "member")
 public class Member extends BaseEntity {
 
+    @JsonIgnore
     @Id
     @GeneratedValue
     @Column(name = "member_id")
@@ -30,11 +39,16 @@ public class Member extends BaseEntity {
     @Column(name = "member_email", unique = true)
     private String email;//회원 이메일
 
+    @JsonIgnore
     @Column(name = "member_pw", nullable = false)
     private String password;//회원비밀번호
 
     @Column(name = "member_name", nullable = false)
     private String name;//회원이름
+
+    @JsonIgnore
+    @Column(name = "activated")
+    private boolean activated;
 
     private boolean emailVerified;//이메일 인증 절차 위해 사용
     
@@ -51,6 +65,13 @@ public class Member extends BaseEntity {
     @Embedded
     @Column(nullable = false)
     private Address address;//회원 주소
+
+    @ManyToMany
+    @JoinTable(
+        name = "member_authority",
+        joinColumns = {@JoinColumn(name = "member_id", referencedColumnName = "member_id")},
+        inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
 
     @Builder
     public Member(String email, String password, String name, String telephone, Address address) {
